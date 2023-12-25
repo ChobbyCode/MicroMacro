198287a01c11703135f481f6bc85a28cef1a4ce8
ChobbyCode <jacobhadfield99@gmail.com>
2023-12-25 16:43:04 +0000

    frfr I forgot what I changed

---
+++ MicroCacheManager/CacheInitilizer.cs
@@ -0,0 +1,13 @@
+﻿using Slide;
+
+namespace MicroCacheManager
+{
+    public class CacheInitilizer
+    {
+        public CacheInitilizer()
+        {
+            Cache cache = new Cache("NewCache", "Bob", "MicroMacro", "C");
+            cache.WriteCache();
+        }
+    }
+}
