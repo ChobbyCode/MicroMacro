36ebd80376249efc06fbdfbe815533433f2a2f6a
ChobbyCode <jacobhadfield99@gmail.com>
2023-12-27 11:18:45 +0000

    File Download

---
+++ MicroMacroInstaller/FileDownloader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroMacroInstaller
+{
+    public class FileDownloader
+    {
+        public void DownloadFiles()
+        {
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+
+            string path = "https://github.com/ChobbyCode/Update-Checker" + @"/zipball/Download";
+            Console.Write("Fetching Data From: ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(path);
+            Console.ForegroundColor = ConsoleColor.White;
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(path, baseDir + @"\Files\download.zip");
+            }
+        }
+    }
+}
